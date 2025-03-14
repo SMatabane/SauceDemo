46f534abe8d026af05477cfde214f9ec9f87c093
Mankgethwa Matabane <mankgethwa.matabane@nagarro.com>
2025-03-14 09:16:15 +0200

    update

--- src/test/java/com/prime/testcases/LoginTest.java
+++ src/test/java/com/prime/testcases/LoginTest.java
@@ -63,6 +63,7 @@ public class LoginTest extends BaseClass {
         } else {
 
             assertTrue(home.areProductsDisplayed(),"Items not displayed");
+            login.LogOut();
         }
 
 
